fac980bb78eff7cdaa9898043846ed78a3665d94
Filip Rudek <rudek.filip@gmail.com>
2020-10-23 14:49:09 +0200

    first commit

---
+++ src/js/_Card.js
@@ -0,0 +1,9 @@
+console.log("card");
+console.log("Zaimportowane z _Card.js");
+
+
+class Card {
+    constructor(){
+        
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// ===Style===
+import "../scss/style.scss";
+import "bootstrap";
+
+// ===Images===
+
+import "../images/Logo.svg";
+import "../images/plus-solid.svg";
+
+// ===JS===
+import "./_Card";
