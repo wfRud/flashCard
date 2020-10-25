acb9cb63e41bdb7e34ea658e98b79e96e0cd2bf5
Filip Rudek <rudek.filip@gmail.com>
2020-10-25 19:41:24 +0100

    styled and animated card_panel collapsed

--- src/js/_customAnimation.js
+++ src/js/_customAnimation.js
@@ -1,6 +1,13 @@
 window.onload = (function () {
   const navigationBurger = document.querySelector(".navigation__burger");
   const navigationList = document.querySelector(".navigation__list");
+  const cardPanelAddBtn = document.querySelector(".card_panel__add__icon");
+  const cardPanelForm = document.querySelector(".card_panel__form");
+
+  cardPanelAddBtn.addEventListener("click", () => {
+    cardPanelForm.classList.toggle("expanded");
+  });
+
   navigationBurger.addEventListener("click", () => {
     navigationBurger.classList.toggle("active");
     navigationList.classList.toggle("active");
