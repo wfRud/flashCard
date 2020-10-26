834e9a4bf833cd36beee65feef286090c8a43b9a
Filip Rudek <rudek.filip@gmail.com>
2020-10-26 16:04:07 +0100

    styled pagination

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,8 @@ import "bootstrap";
 import "../images/Logo.svg";
 import "../images/plus-solid.svg";
 import "../images/heart-solid.svg";
+import "../images/angle-left-solid.svg";
+import "../images/angle-right-solid.svg";
 
 // ===JS===
 import "./_customAnimation";
